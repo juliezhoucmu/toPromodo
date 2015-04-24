3508854ebb190f2d23499f51e8acd466607ed454
miisme <{zhoumi@ustc.edu}>
2015-04-24 18:51:49 -0400

    给自定义的TableViewCell加上了pan手势，效果是：向左划，超过一半的这一格会变空，否则弹回来

--- toPromodo/TableViewCell.swift
+++ toPromodo/TableViewCell.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class TableViewCell: UITableViewCell {
+    
+    //下面的两个property为pan gesture recognizer 所用
+    var originalCenter = CGPoint()
+    var deleteOnDragRelease = false //确定该次手势是否需要产生Delete的动作
 
 
     //UITableViewCell 继承自UIView， 而UIView conforms to Protocol NSCoding, 所以必须要有一个这样的init函数，这里其实不写这个也没事
@@ -19,6 +23,10 @@ class TableViewCell: UITableViewCell {
     
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        // 为自定义的cell定义pan手势 recognizer ，实现：右划 delete的动作
+        var recognizer = UIPanGestureRecognizer(target:self,action: "handlePan:")
+        recognizer.delegate = self //一条龙的自产自销，这一样必须在自己的类里面定义handlePan函数了
+        addGestureRecognizer(recognizer) //加到自己身上
     }
 
     override func awakeFromNib() {
@@ -31,5 +39,30 @@ class TableViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
+    
+    func handlePan(recognizer: UIPanGestureRecognizer) {
+        if recognizer.state == .Began {
+            originalCenter = center //表示该cell所在view的center
+        }
+        
+        if recognizer.state == .Changed {
+            
+            let translation = recognizer.translationInView(self)
+            center = CGPointMake(originalCenter.x + translation.x, originalCenter.y)
+            deleteOnDragRelease  = frame.origin.x < -frame.size.width / 2.0 //左划超过一半才算
+        }
+        
+        if recognizer.state == .Ended {
+            let originalFrame = CGRect(x: 0, y: frame.origin.y, width: bounds.size.width, height: bounds.size.height)
+            
+            if !deleteOnDragRelease { //恢复原状
+                UIView.animateWithDuration(0.2, animations: {self.frame = originalFrame})
+            }
+            
+        }
+        
+        
+    }
+
 
 }
